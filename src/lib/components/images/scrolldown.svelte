15095d0d6091c39f05415ce7cb6e6a355ea3eacd
Bradley Rastrullo <11504992+brastrullo@users.noreply.github.com>
2023-09-25 08:24:55 -0400

    fix error

--- src/lib/components/images/scrolldown.svelte
+++ src/lib/components/images/scrolldown.svelte
@@ -1,6 +1,6 @@
 <script>
 	import Chevron from '$lib/components/images/chevron.svelte';
-	export let className = ''
+	export const className = ''
 </script>
 <span class="z-50 absolute bottom-14 left-1/2 -translate-x-1/2 m-auto animate-pulse text-indigo-300 text-xs">scroll</span>
 <div class="fixed m-auto bottom-12 left-1/2 -translate-x-1/2 translate-y-3 flex pointer-events-none">
